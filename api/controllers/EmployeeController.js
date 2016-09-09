1bf4509a89ad4ee9501d8d433a9a90286e10b6d8
Forrest <forrest.corry@gmail.com>
2016-09-09 16:00:38 -0400

    final commit

--- api/controllers/EmployeeController.js
+++ api/controllers/EmployeeController.js
@@ -7,7 +7,7 @@
 
 var Client = require('node-rest-client').Client;
 var client = new Client();
-var endpoint = "http://localhost:1337/employee"
+var endpoint = "http://localhost:1337/users"
 
 module.exports = {
 
@@ -19,10 +19,11 @@ module.exports = {
         if(req.method != "POST"){
           return res.view('create');
         }
-
+        
+  
         var args = {
-            data: req.body,
-            headers: { "Content-Type": "application/json" }
+              data: req.body,
+              headers: { "Content-Type": "application/json" }
         };
          
         client.post(endpoint, args, function (data, response) {
@@ -44,9 +45,9 @@ module.exports = {
   read: function (req, res) {
 
     client.get(endpoint, function (data, response) {
-        return res.view('read', {employees: data});
+        return res.view('read', {users: data});
     }).on('error', function (err) {
-        return res.view('read', {error: { message: "There was an error getting the employees"}});
+        return res.view('read', {error: { message: "There was an error getting the users"}});
     });
 
   },
@@ -56,19 +57,56 @@ module.exports = {
    * `EmployeeController.update()`
    */
   update: function (req, res) {
-    return res.json({
-      todo: 'update() is not implemented yet!'
+   
+   if(req.method != "POST"){
+          client.get(endpoint, function (data, response) {
+          return res.view('update', {users: data});
+    }).on('error', function (err) {
+        return res.view('update', {error: { message: "There was an error getting the users"}});
     });
+  }else{
+        var args = {
+            data: req.body,
+            headers: { "Content-Type": "application/json" }
+        };
+        client.put(endpoint + "/" + req.body.id, args, function (data, response) {
+            // return res.view('create', {success: { message: "Record added successfully"}});
+            if(response.statusCode != "200"){
+                return res.view('update', {error:{message: response.statusMessage + ": " + data.reason}});
+            }
+            return res.redirect('back');
+            return res.view('update', {success:{message: "Record updated successfully"}});
+
+          })
+    }
   },
+
 
 
   /**
    * `EmployeeController.delete()`
    */
   delete: function (req, res) {
-    return res.json({
-      todo: 'delete() is not implemented yet!'
-    });
-  }
-};
+      if(req.method != "POST"){
+          client.get(endpoint, function (data, response){
+        return res.view('delete', {users: data});
+      }).on('error', function (err) {
+        return res.view('delete', {error: { message: "There was an error getting the employees"}});
+       });
+       }else{
+        var args = {
+            data: req.body,
+            headers: { "Content-Type": "application/json" }
+        };
+        client.delete(endpoint + "/" + req.body.id, args, function (data, response) {
+            // return res.view('create', {success: { message: "Record added successfully"}});
+            if(response.statusCode != "200"){
+                return res.view('delete', {error:{message: response.statusMessage + ": " + data.reason}});
+            }
 
+            return res.view('delete', {success:{message: "Record created successfully"}});
+
+        })
+      }
+    }
+  }
